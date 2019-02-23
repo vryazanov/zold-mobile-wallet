5c267583678b7bf4d9b9eb15f5323c8ac9e90081
Vadim Ryazanov <vadimvoronezh@yandex.ru>
2019-02-23 17:15:21 +0300

    Add zold-node-sdk and react-native-phone-input to requirements

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StyleSheet, Text, TextInput, View, Image } from 'react-native'
+import PhoneInput from 'react-native-phone-input'
+
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Image source={require('./assets/zold.png')} style={styles.logo} />
+        <View style={styles.labelContainer}>
+          <Text>Enter your phone number:</Text>
+        </View>
+        <View style={styles.inputContainer}>
+          <PhoneInput />
+        </View>
       </View>
     );
   }
@@ -14,8 +22,20 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginTop: 100,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+  },
+  logo: {
+    width: 50,
+    height: 50,
+  },
+  inputContainer: {
+    width: 150,
+    marginTop: 25,
+  },
+  labelContainer: {
+    marginTop: 20,
   },
 });
