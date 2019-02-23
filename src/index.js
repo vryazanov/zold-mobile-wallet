a8a7c2c454309e496da9631fda9384f6c49b1396
Vadim Ryazanov <vadimvoronezh@yandex.ru>
2019-02-23 18:09:23 +0300

    Add forgottent files

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import { registerRootComponent } from 'expo'
+
+import App from './App'
+
+export default Expo.registerRootComponent(App);
---
+++ src/routes.js
@@ -0,0 +1,5 @@
+/*
+@todo #2:30min I've added `react-navigation` package to requirements. Introduce here routes
+ and their components for the following pages: policy, terms of use. Find the way how to
+ show texts from https://www.zerocracy.com/terms.html and https://www.zerocracy.com/policy.html
+*/
